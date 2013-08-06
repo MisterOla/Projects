928e4b4a13d8d360f2ba8b58e9fa368ae7dd6871
Michael Herman <hermanmu@gmail.com>
2013-08-06 00:06:33 -0500

    Update prime.py

--- Numbers/prime.py
+++ Numbers/prime.py
@@ -1,24 +1,21 @@
-# Prime Factorization - Have the user enter a number
-# and find all Prime Factors (if there are any) and
-# display them.
-
 import math
-
-def is_a_prime(x):
-    for i in range(2, x):
-        if x % i == 0:
-            return False
+ 
+def prime(n):
+    i = 2
+    while i < 0.5 + math.sqrt(n):
+      if n % i == 0:
+        return False
+      i+=1
     return True
-
-# standard boilerplate
-if __name__ == '__main__':
-    n = int(raw_input('Enter the number to find prime factors of: '))
-
+ 
+def primefactors(n):
     factors = []
+    for i in range(n/2, 0, -1):
+      if prime(i) and n % i == 0:
+        factors.append(i)
+    return factors
 
-    for i in range(2, n + 1):
-        while n % i == 0: # Thanks @madsulrik
-            if is_a_prime(i):
-                factors.append(i)
-                n /= i
-    print factors
+# driver code
+print primefactors(9)   == [3, 1]
+print primefactors(21)  == [7, 3, 1]
+print primefactors(156) == [13, 3, 2, 1]

410a2e86e33a76ccd3294f7fbe32ef8dec2f409d
Michael Herman <hermanmu@gmail.com>
2013-08-05 23:11:08 -0500

    Update pi.py

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,10 +1,6 @@
 # Find PI to the Nth Digit
 
-from math import *
+puts "Enter the number of decimal places you would like: "
+decimals = gets.chomp
 
-digits = raw_input('Enter number of digits to round PI to: ')
-
-# print ('{0:.%df}' % min(20, int(digits))).format(math.pi) # nested string formatting
-
-# calculate pi using Machin-like Formula
-print ('{0:.%df}' % min(30, int(digits))).format(4 * (4 * atan(1.0/5.0) - atan(1.0/239.0)))
+puts Math::PI.round(decimals.to_i)
